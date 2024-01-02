d0648533ee65a57a3a7bb0772e05012eadb1e904
Prathmesh-Kolekar <128283076+Prathmesh-Kolekar@users.noreply.github.com>
2024-01-02 20:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 import streamlit as st
 import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-# from langchain.schema.output_parser import StrOutputParser
 from langchain.prompts.prompt import PromptTemplate
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationChain
 
+# Load environment variables from .env
+load_dotenv()
 
+# Set the API key
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', default="your_default_api_key")
+genai.configure(api_key=GOOGLE_API_KEY)
 
-# model = genai.GenerativeModel("gemini-pro")
-
-# output_parser = StrOutputParser()
-
-
-os.environ['GOOGLE_API_KEY'] = "AIzaSyBmZtXjJgp7yIAo9joNCZGSxK9PbGMcVaA"
-genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
-
-model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.5)
+model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5)
 
 template = """The following is a friendly conversation between a human and an AI. The AI named Shree is talkative and provides lots of specific details from its context which can also serve as conversation history to remember previous responses and dont generate anything else if you dont know say so.
 
@@ -29,16 +25,14 @@ Human: {input}
 AI Assistant:"""
 PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
 
-
 st.title("Shree-v01")
-
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {
-            "role":"assistant",
-            "content":"Greetings , How may I help you"
+            "role": "assistant",
+            "content": "Greetings , How may I help you"
         }
     ]
 
@@ -58,19 +52,17 @@ conversation = ConversationChain(
 )
 
 # Process and store Query and Response
-
 def llm_function(query):
     ans = conversation.predict(input=query)
-    # print(conversation.memory)
+
     # Displaying the Assistant Message
     with st.chat_message("assistant"):
         st.markdown(ans)
 
-
     # Storing the User Message
     st.session_state.messages.append(
         {
-            "role":"user",
+            "role": "user",
             "content": query
         }
     )
@@ -78,16 +70,13 @@ def llm_function(query):
     # Storing the User Message
     st.session_state.messages.append(
         {
-            "role":"assistant",
+            "role": "assistant",
             "content": ans
         }
     )
 
-
-   
 # Accept user input
 query = st.chat_input("Enter Your Message")
-
 
 # Calling the Function when Input is Provided
 if query:
